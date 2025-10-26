885ba334221b9b390523afb7127060a6e7df49ba
Daniel <zoe19902@gmail.com>
2025-10-26 14:49:38 -0500

    v33

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => res.json({ status: 'ok', ts: Date.now() }));
-app.get('/hello', (req, res) => res.send(`Hello world from argo v2 ${process.env.SERVICE_NAME || 'microservice'}`));
+app.get('/hello', (req, res) => res.send(`Hello world from argo v3 ${process.env.SERVICE_NAME || 'microservice'}`));
 
 app.listen(port, () => console.log(`Listening ${port}`));
